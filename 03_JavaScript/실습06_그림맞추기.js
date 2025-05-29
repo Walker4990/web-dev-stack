50bc41c61c059c27e7b8cf0b9c62729dffbc6e2a
Walker4990 <bora0193@naver.com>
2025-05-29 12:06:30 +0900

    .

--- 03_JavaScript/실습06_그림맞추기.js
+++ 03_JavaScript/실습06_그림맞추기.js
@@ -1,18 +1,47 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const box1 = document.querySelector(".box1");
-  console.log(box1);
-  let count = 1;
-  box1.addEventListener("click", () => {
-    box1.innerHTML = `Click ${count++}`;
-    const img = document.querySelectorAll("img"); // 3개
-    console.log(img);
+const click = document.querySelector("section button:first-child");
+const img = document.querySelectorAll("img");
+const span = document.querySelector("button span");
+const h2 = document.querySelector("h2");
+const restart = document.querySelector("section button:last-child");
+console.log(img);
+// click : 클릭 이벤트를 걸어야 하는 애
+// img : 내가 클릭을 했을 때 바꿔야 하는 애
+let count = 0;
+const game = () => {
+  // 랜덤값  : 1 ~ 3까지
+  const random = [
+    Math.floor(Math.random() * 3) + 1,
+    Math.floor(Math.random() * 3) + 1,
+    Math.floor(Math.random() * 3) + 1,
+  ];
+  console.log(random);
+  for (let i = 0; i < img.length; i++) {
+    img[i].setAttribute("src", `assets/spy${random[i]}.jpg`);
+  }
+  // 클릭할 때마다 카운트 증가
+  span.innerHTML = ++count;
 
-    for (const item of img) {
-      const random1 = Math.floor(Math.random() * 3 + 1);
-      item.setAttribute("src", `assets/spy${random1}.jpg`);
-    }
-  });
-  // 클릭을 누르면 이미지가 세개가 랜덤출력
-
-  // 속성을 변경 setAttribute("속성", "값")
-});
+  // 이미지 3개가 일치한 경우 버튼 disabled 처리
+  //click.setAttribute("disabled", "disabled");
+  //click.setAttribute("disabled", true);
+  if (random[0] === random[1] && random[1] === random[2]) {
+    click.disabled = true;
+    // h2에 visibility: visible로 변경
+    h2.style.visibility = "visible";
+  }
+};
+const end = () => {
+  // 이미지는 처음 그대로 1, 2, 3 순서대로 보이게 하고
+  for (let i = 0; i < img.length; i++) {
+    img[i].setAttribute("src", `assets/spy${i + 1}.jpg`);
+  }
+  // count는 0으로 바꾸고 span은 비우고
+  count = 0;
+  span.innerHTML = "";
+  // h2 visibility = hidden 처리
+  h2.style.visibility = "hidden";
+  // click 버튼의 disabled false 처리
+  click.disabled = false;
+};
+click.addEventListener("click", game);
+restart.addEventListener("click", end);
